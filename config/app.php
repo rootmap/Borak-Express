c83bf889a4fe23bc0ac6c43d6368db90cf82d0c8
Muntashir Are rahi <muntashir.rahi@gmail.com>
2021-06-23 13:27:10 +0600

    Spatie Permissions added

--- config/app.php
+++ config/app.php
@@ -178,6 +178,7 @@ return [
         App\Providers\RouteServiceProvider::class,
         Maatwebsite\Excel\ExcelServiceProvider::class,
        //Meneses\LaravelMpdf\LaravelMpdfServiceProvider::class,
+        Spatie\Permission\PermissionServiceProvider::class,
 
     ],
 
